69703dce8b54524e45acf51863b83eae37d242f4
sidharth nayyar <sidnayyar1@gmail.com>
2019-07-20 02:31:40 -0400

    [Sidharth] added view controller in storyboard for profile view controller and contact us view controller

---
+++ ITBookStore/ContactUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactUsViewController.swift
+//  ITBookStore
+//
+//  Created by Sidharth Nayyar on 2019-07-20.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
